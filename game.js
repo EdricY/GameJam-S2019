fd99280453579140144d0ebe55b851b97139c7cf
eyu <eyu@cse21708.cs.local>
2019-04-05 18:40:30 -0400

    the beginning

---
+++ game.js
@@ -0,0 +1,37 @@
+var canvas = document.getElementById('canvas')
+var ctx = canvas.getContext('2d')
+
+const UPDATES_PER_SEC = 30;
+const MS_PER_UPDATE = 1000 / UPDATES_PER_SEC;
+var lastTime = Date.now();
+var lag = 0;
+var redraw = false;
+
+function gameDraw() {
+  ctx.clearRect(0, 0, canvas.width, canvas.height);
+  ctx.fillStyle = "red"
+  ctx.fillRect(100, 100, 20, 10)
+
+}
+
+function gameUpdate() {
+
+}
+
+function tick() {
+  let current = Date.now();
+  let elapsed = current - lastTime;
+  lastTime = current;
+  lag += elapsed;
+  while (lag >= MS_PER_UPDATE) {
+    gameUpdate();
+    lag -= MS_PER_UPDATE;
+    redraw = true;
+  }
+  if (redraw) {
+    gameDraw();
+    redraw = false;
+  }
+  requestAnimationFrame(tick);
+}
+requestAnimationFrame(tick);
